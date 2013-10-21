3178dab18e540cf10bf3febda46766d9dde1c1b2
Roga <roga7zl@gmail.com>
2013-10-21 14:35:03 +0900

    Modify app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
  * Module dependencies.
  */
 
+// TODO DBとかにする
+var value1 = -1;
+var value2 = -1;
+
 var express = require('express');
 var routes = require('./routes/main.js');
 var user = require('./routes/user');
@@ -10,6 +14,7 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
+var server = http.createServer(app);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -30,6 +35,24 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+// Ready for Socket.io
+var socket = server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+//----------------------------------------------------------------------//
+
+// 値の変更を受け取ったとき
+socket.on('connection', function(client) {
+  // connect
+  client.on('value update', function(decimal) {
+    value1 = decimal;
+    console.log('device1 receive: ' + value1);
+
+    socket.emit('value change', value1);
+    socket.broadcast.emit('value change', value1);
+  });
+  client.on('disconnect', function() {
+    // disconnect
+  });
 });
