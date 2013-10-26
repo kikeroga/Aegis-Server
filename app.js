d41cafd96aeb97b10c9c0a6cda848d71bddd671b
Roga <roga7zl@gmail.com>
2013-10-27 03:17:41 +0900

    Refactoring

--- app.js
+++ app.js
@@ -1,74 +1,46 @@
-
-/* Configuration */
+const PORT = 3000;
 
 // TODO DBとかにする
 var value1 = -1;
 var value2 = -1;
 
 var express = require('express');
-var routes = require('./routes/main.js');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-// var $ = require('jquery');
+
 var app = express();
-var server = http.createServer(app);
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
+app.set('port', process.env.PORT || PORT);
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
-//----------------------------------------------------------------------//
-
-app.get('/', routes.index);
-// app.get('/users', user.list);
-
+var server = http.createServer(app);
 var socket = server.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+// Receive
 var io = require('socket.io').listen(socket);
-
 io.sockets.on('connection', function(client) {
-    console.log('connected by: ' + client.id);
+    console.log(client.id + ' connected');
 
-    client.on('valueUpdate', function(decimal) {
-        value1 = decimal;
-        console.log('device1 receive: ' + value1);
+    //client.on('change', function(decimal) {
+    client.on('change', function(decimal) {
+        var value = decimal;
+        console.log(client.id + ': ' + value);
 
-/*
-        // $('#device1').html(value1);
-        $('#device1').prepend(value1);
-        console.log('Set value');
-*/
-
-/*
-        client.emit('valueChange', value1);
-        //socket.broadcast.emit('value change', value1);
-*/
-        client.emit('valueChange', value1);
-        //socket.broadcast.emit('value change', value1);
+        client.emit('update', value);
+        client.broadcast.emit('update', value);
     });
 
     client.on('disconnect', function() {
-        // disconnect
         console.log('disconnected by: ' + client.id);
     });
 });
 
+// heroku用に必要な処理らしい
 io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
 });
-
