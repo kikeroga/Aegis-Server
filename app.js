8403815b949055a345b826b9f54a999be9c94d98
Roga <roga7zl@gmail.com>
2013-11-04 07:36:09 +0900

    Modify app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const PORT = 3000;
 
-// TODO DBとかにする
+// TODO とりあえず変数だけど将来的にはDB
 var value1 = -1;
-//var value2 = -1;
+var value2 = -1;
 
 var express = require('express');
 var http = require('http');
@@ -24,17 +24,32 @@ var socket = server.listen(app.get('port'), function(){
 var io = require('socket.io').listen(socket);
 io.sockets.on('connection', function(client) {
     console.log(client.id + ' connected');
-    client.emit('init', value1);
+    client.emit("init", getValuesJson());
 
-    //client.on('change', function(decimal) {
-    client.on('change', function(decimal) {
-        console.log(client.id + ': ' + decimal);
+    client.on('change', function(value) {
+        console.log(client.id + ': ' + value);
 
         // update
-        value1 = decimal;
+        var json;
+        try {
+            json = JSON.parse(value); 
+        } catch(e) {
+            console.log(e);
+            return;
+        }
 
-        client.emit('update', decimal);
-        client.broadcast.emit('update', decimal);
+        if ('device1' == json.id) {
+            value1 = json.value;
+        } else if ('device2' == json.id) {
+            value2 = json.value;
+        } else {
+            return ;
+        }
+
+        var message = getValuesJson();
+
+        client.emit('update', message);
+        client.broadcast.emit('update', message);
     });
 
     client.on('disconnect', function() {
@@ -42,8 +57,16 @@ io.sockets.on('connection', function(client) {
     });
 });
 
+function getValuesJson() {
+    return '{"device1" : ' + value1 + ','
+          + '"device2" : ' + value2 + '}';
+}
+
+/*
 // heroku用
 io.configure(function () { 
     io.set("transports", ["xhr-polling"]); 
     io.set("polling duration", 10); 
 });
+*/
+
